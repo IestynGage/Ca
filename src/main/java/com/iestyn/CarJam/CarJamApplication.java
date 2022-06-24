f8617ac7b4ffc80619d6c851a529eda5da2e6436
Iestyn Gage <Iestyn.Gage@talkout.com>
2022-06-24 23:36:45 +0100

    Init project

---
+++ src/main/java/com/iestyn/CarJam/CarJamApplication.java
@@ -0,0 +1,13 @@
+package com.iestyn.CarJam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarJamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarJamApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/iestyn/CarJam/CarJamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iestyn.CarJam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarJamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
